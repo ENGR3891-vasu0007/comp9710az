979fa917f5c13369198fc67fb64a7b144af2e939
lchjames <apswsttss@gmail.com>
2021-10-23 22:06:04 +1030

    Update DBConnect.php

--- DBConnect.php
+++ DBConnect.php
@@ -15,5 +15,7 @@ mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
 //If connection failed, show the error
 if (mysqli_connect_errno()) {
     die('Failed to connect to MySQL: ' . mysqli_connect_error());
+} else {
+    echo "Connect to mySQL successfully <br/>";
 }
 ?>
