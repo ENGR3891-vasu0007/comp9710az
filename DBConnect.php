ae848c710def66381203799ef7beafbf85e2d3c5
lchjames <apswsttss@gmail.com>
2021-10-23 20:27:54 +1030

    Update DBConnect.php

--- DBConnect.php
+++ DBConnect.php
@@ -1,3 +1,18 @@
 <?php
 
+#DBConnect.php
+$host = 'mytestserver2.mysql.database.azure.com';
+$username = 'vasulg';
+$password = 'Flinders@';
+$db_name = 'comp9710';
+
+//Establishes the connection
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "./sql/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+if (mysqli_connect_errno($conn)) {
+    die('Failed to connect to MySQL: ' . mysqli_connect_error());
+} else {
+    echo "Connected";
+}
 ?>
