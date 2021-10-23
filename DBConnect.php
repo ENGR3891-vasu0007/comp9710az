2c61c284d3130ba574f444fd79e643afd2afc1d3
lchjames <apswsttss@gmail.com>
2021-10-23 20:22:36 +1030

    update to DBConnect.php

--- DBConnect.php
+++ DBConnect.php
@@ -11,5 +11,7 @@ $conn = mysqli_init();
 mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
 if (mysqli_connect_errno($conn)) {
     die('Failed to connect to MySQL: ' . mysqli_connect_error());
+} else {
+    echo "Connected";
 }
 ?>
