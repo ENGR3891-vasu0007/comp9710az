0ca260856ab7e82b05b6ef6fe9b2b6005c674c6a
lchjames <apswsttss@gmail.com>
2021-10-23 22:09:51 +1030

    test update

--- DBConnect.php
+++ DBConnect.php
@@ -10,7 +10,7 @@ $conn = mysqli_init();
 mysqli_ssl_set($conn, NULL, NULL, "cacert.pem", NULL, NULL);
 
 // Establish the connection
-mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
 
 //If connection failed, show the error
 if (mysqli_connect_errno()) {
