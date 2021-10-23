d4eced24d0ec10f0f386fb38380eb668cc18e325
lchjames <apswsttss@gmail.com>
2021-10-24 07:59:21 +1030

    Update user_register_login.php

--- user_register_login.php
+++ user_register_login.php
@@ -107,7 +107,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['register_submit'])) {
             $LusernameErr = "User not exists";
         }
         $conn->close();
-        echo "SESSION[loggedin]" . $_SESSION["loggedin"];
+        echo "SESSION[username]" . $_SESSION["username"];
     }
 }
 ?>
