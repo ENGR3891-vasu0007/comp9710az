c87f2ab314e799cc9b6bbdc0476b18e1024c49a2
lchjames <apswsttss@gmail.com>
2021-10-24 08:05:39 +1030

    Update topbar.php

--- topbar.php
+++ topbar.php
@@ -4,6 +4,7 @@ include 'user_register_login.php';
 ?>
 <html>
     <head>
+        <title>User page</title>
         <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
         <link rel="stylesheet" type="text/css" href="css/styleBG.css"/>
