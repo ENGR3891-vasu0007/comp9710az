5691798407d0a95a6e7965d99638b2e1044a1dd1
lchjames <apswsttss@gmail.com>
2021-10-23 21:01:34 +1030

    test

--- DBConnect.php
+++ DBConnect.php
@@ -6,8 +6,11 @@ $username = 'vasulg';
 $password = 'Flinders@';
 $db_name = 'comp9710';
 
-//Establishes the connection
+//Initializes MySQLi
 $conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+//Establishes the connection
+
 mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
 if (mysqli_connect_errno($conn)) {
     die('Failed to connect to MySQL: ' . mysqli_connect_error());
