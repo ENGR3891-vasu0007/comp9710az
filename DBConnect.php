930a55e9bd5ddcb5999507bb44b35490e5b0df9f
lchjames <apswsttss@gmail.com>
2021-10-24 16:49:09 +1030

    Update DBConnect.php

--- DBConnect.php
+++ DBConnect.php
@@ -7,7 +7,7 @@ $password = 'Flinders@';
 $db_name = 'comp9710';
 $conn = mysqli_init();
 
-mysqli_ssl_set($conn, NULL, NULL, "cacert.pem", NULL, NULL);
+mysqli_ssl_set($conn, NULL, NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
 
 // Establish the connection
 mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
