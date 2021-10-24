870f0a4e1c6e906914fcd785d25d2a91282f082c
lchjames <apswsttss@gmail.com>
2021-10-24 16:33:04 +1030

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,13 +2,6 @@
 
 // Initialize the session
 session_start();
-//Confirm to set machine_state off
-include 'DBConnect.php';
-$sql = "UPDATE user_account SET machine_state = 0 WHERE `username` = '" . $_SESSION["username"] . "'";
-if ($conn->query($sql) === FALSE) {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-$conn->close();
 // Unset all of the session variables
 $_SESSION = array();
 
