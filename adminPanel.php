a8a8a76b17b896b6024f8b24d643c46226ab023f
lchjames <apswsttss@gmail.com>
2021-10-27 20:25:34 +1030

    Update adminPanel.php

--- adminPanel.php
+++ adminPanel.php
@@ -1,7 +1,10 @@
-    <?php
-    include 'topbar.php';
-    include 'updateModuleDetail.php';
-    ?>
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+include 'topbar.php';
+include 'updateModuleDetail.php';
+?>
 <html>
     <head>
         <meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
@@ -10,17 +13,17 @@
         <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js'></script>
     </head>
     <body>
-    <div id="page-wrap">
+        <div id="page-wrap">
             <header>
                 <h1>Admin Panel</h1>
                 <nav>
                     <ul class="group">
-                     <li><a href="editModules.php">Edit Modules</a></li>
-                     <li><a href="editActivity.php">Edit Activities</a></li>
-                     <li><a href="editUser.php">Edit User</a></li>
+                        <li><a href="editModules.php">Edit Modules</a></li>
+                        <li><a href="editActivity.php">Edit Activities</a></li>
+                        <li><a href="editUser.php">Edit User</a></li>
                     </ul>
                 </nav>
             </header>
         </div>
-</body>
+    </body>
 </html>
